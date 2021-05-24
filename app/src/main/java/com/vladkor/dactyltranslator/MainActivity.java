816c1c009ecb26b65bef9917c943a12ba8871a66
Vlad <vladkor280@yandex.ru>
2021-05-24 10:26:43 +0300

    Add Bottom Sheet

--- app/src/main/java/com/vladkor/dactyltranslator/MainActivity.java
+++ app/src/main/java/com/vladkor/dactyltranslator/MainActivity.java
@@ -1,14 +1,65 @@
 package com.vladkor.dactyltranslator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout bottomLinearLayout;
+    BottomSheetBehavior bottomSheetBehavior;
+    LinearLayout transtaleTextLayout;
+    ImageView bottomSheetArrow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomLinearLayout = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomLinearLayout);
+        transtaleTextLayout = findViewById(R.id.translate_text);
+        bottomSheetArrow = findViewById(R.id.bottom_sheet_arrow);
+        ViewTreeObserver vto = transtaleTextLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                transtaleTextLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                bottomSheetBehavior.setPeekHeight(transtaleTextLayout.getMeasuredHeight());
+            }
+        });
+
+        bottomSheetBehavior.setHideable(false);
+        bottomSheetBehavior.addBottomSheetCallback(
+                new BottomSheetBehavior.BottomSheetCallback() {
+                    @Override
+                    public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                        switch (newState){
+                            case BottomSheetBehavior.STATE_HIDDEN:
+                                break;
+                            case BottomSheetBehavior.STATE_EXPANDED:
+                                //bottomSheetArrow.setImageResource(R.drawable);
+                                break;
+                            case BottomSheetBehavior.STATE_COLLAPSED:
+                                //bottomSheetArrow.setImageResource();
+                                break;
+                            case BottomSheetBehavior.STATE_DRAGGING:
+                                break;
+                            case BottomSheetBehavior.STATE_SETTLING:
+                                //bottomSheetArrow.setImageResource();
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+                    }
+                }
+        );
     }
 }
