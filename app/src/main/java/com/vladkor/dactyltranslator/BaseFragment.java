b9355b2dcf6f95426862a14bc0c1915110894122
Vlad <vladkor280@yandex.ru>
2021-05-25 21:50:31 +0300

    (DONT WORKED) Start add Auth System

--- app/src/main/java/com/vladkor/dactyltranslator/BaseFragment.java
+++ app/src/main/java/com/vladkor/dactyltranslator/BaseFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,13 +53,22 @@ public class BaseFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            nameS = getArguments().getString("NAME");
         }
     }
-
+    private String nameS;
+    private TextView name;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false);
+        View v = inflater.inflate(R.layout.fragment_base, container, false);
+        name = v.findViewById(R.id.nameTextView);
+        if (nameS != null){
+            name.setText(nameS);
+        }
+
+
+        return v;
     }
 }
--- app/src/main/java/com/vladkor/dactyltranslator/LessonsActivity.java
+++ app/src/main/java/com/vladkor/dactyltranslator/LessonsActivity.java
@@ -1,14 +1,50 @@
 package com.vladkor.dactyltranslator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class LessonsActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+public class LessonsActivity extends AppCompatActivity {
+    private Fragment f1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AccountManager manager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+        Account[] list = manager.getAccounts();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+        }
+
         setContentView(R.layout.activity_lessons);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("NAME", list[0].name);
+        f1 = new BaseFragment();
+        f1.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment, f1);
+        fragmentTransaction.commit();
+
+
     }
 }
