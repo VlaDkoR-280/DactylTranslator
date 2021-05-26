6afb5570553c52d0ea41f2aa99be540b39cc33f0
Vlad <vladkor280@yandex.ru>
2021-05-26 22:05:57 +0300

    Redesign on CardView's elements

--- app/src/main/java/com/vladkor/dactyltranslator/LessonsActivity.java
+++ app/src/main/java/com/vladkor/dactyltranslator/LessonsActivity.java
@@ -1,28 +1,22 @@
 package com.vladkor.dactyltranslator;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.net.Uri;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class LessonsActivity extends AppCompatActivity {
-    private Fragment f1;
+    private ProfilePersonFragment f1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        f1 = new BaseFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.fragment, f1);
+        f1 = new ProfilePersonFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, f1);
         fragmentTransaction.commit();
 
 
--- app/src/main/java/com/vladkor/dactyltranslator/ProfilePersonFragment.java
+++ app/src/main/java/com/vladkor/dactyltranslator/ProfilePersonFragment.java
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BaseFragment#newInstance} factory method to
+ * Use the {@link ProfilePersonFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BaseFragment extends Fragment {
+public class ProfilePersonFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +24,7 @@ public class BaseFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public BaseFragment() {
+    public ProfilePersonFragment() {
         // Required empty public constructor
     }
 
@@ -35,11 +34,11 @@ public class BaseFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BaseFragment.
+     * @return A new instance of fragment ProfilePersonFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static BaseFragment newInstance(String param1, String param2) {
-        BaseFragment fragment = new BaseFragment();
+    public static ProfilePersonFragment newInstance(String param1, String param2) {
+        ProfilePersonFragment fragment = new ProfilePersonFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,22 +52,13 @@ public class BaseFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-            //nameS = getArguments().getString("NAME");
         }
     }
-    private String nameS;
-    private TextView name;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_base, container, false);
-        name = v.findViewById(R.id.nameTextView);
-        if (nameS != null){
-            name.setText(nameS);
-        }
-
-
-        return v;
+        return inflater.inflate(R.layout.fff, container, false);
     }
 }
